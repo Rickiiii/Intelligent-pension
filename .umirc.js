f1b16229061e6753ebdb7579c9eded580ddc87d1
Ric <wangrj@weipaitang.com>
2019-04-22 14:37:58 +0800

    框架改为umi

--- .umirc.js
+++ .umirc.js
@@ -1,41 +1,21 @@
+
+// ref: https://umijs.org/config/
 export default {
+  treeShaking: true,
   plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
-      dva: {
-        immer: true,
-      },
-      antd: true,
+      antd: false,
+      dva: false,
+      dynamicImport: false,
+      title: 'Intelligent-pension',
+      dll: false,
+      
       routes: {
-        exclude: [/models\//],
+        exclude: [
+          /components\//,
+        ],
       },
-      polyfills: ['ie9'],
-      locale: {},
-      library: 'react',
-      dynamicImport: {
-        webpackChunkName: true,
-        loadingComponent: './components/Loading.js',
-      },
-      dll: {
-        exclude: [],
-      },
-      hardSource: true,
-      pwa: true,
-      hd: true,
-      fastClick: true,
-      title: 'default title',
-      chunks: ['vendor', 'umi'],
-      scripts: [
-        { src: 'http://cdn/a.js' },
-        { src: '<%= PUBLIC_PATH %>a.js' },
-        { content: `alert('a');` },
-      ],
-      headScripts: [],
-      metas: [
-        { charset: 'utf-8' },
-      ],
-      links: [
-        { rel: 'stylesheet', href: 'http://cdn/a.css' },
-      ],
     }],
   ],
-};
+}
